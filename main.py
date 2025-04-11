1c97361f73ad296574cc6eba6997721c881d5471
juanjo-cell <jcardenas107300@umanizales.edu.co>
2025-04-10 19:01:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ app.include_router(abb.abb_route)
 async def root():
     return {"message": "Hello World"}
 
-
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
